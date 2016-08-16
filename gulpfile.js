399cb4384f2968cbea7296ad5558d9eda35230a0
Rodrigo Vieira <rodrigo@crabbly.com>
2016-08-16 03:12:38 -0400

    add javaScript standard style test and switch gulp tasks to elixir

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,8 @@
+var elixir = require('laravel-elixir')
+elixir.config.assetsPath = 'src'
+elixir.config.js.folder = ''
+elixir.config.css.folder = ''
 
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    cleanCSS = require('gulp-clean-css');
-
-gulp.task('js', function() {
-    return gulp.src('src/print.js')
-        .pipe(uglify())
-        .pipe(rename('print.min.js'))
-        .pipe(gulp.dest('dist/'));
-});
-
-
-gulp.task('css', function() {
-    return gulp.src('src/print.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(rename('print.min.css'))
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('default', ['js', 'css']);
-
-gulp.task('watch', function () {
-    gulp.watch('src/*.js', ['js']);
-});
+elixir(function (mix) {
+  mix.browserify('print.js', 'dist/print.min.js').styles('print.css', 'dist/print.min.css')
+})

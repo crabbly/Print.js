e8918ce6cc6e211ed6aeae42237f483389036692
Rodrigo Vieira <rodrigo@crabbly.com>
2017-04-26 11:21:23 -0400

    removed code styling lib from docs page

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,29 +41,5 @@
 					skel.breakpoint('narrow').active
 				);
 			});
-
 	});
-
-	Rainbow.color();
-
-	someJSONdata = [
-		{
-			name: 'John Doe',
-			email: 'john@doe.com',
-			phone: '111-111-1111'
-		},
-		{
-			name: 'Barry Allen',
-			email: 'barry@flash.com',
-			phone: '222-222-2222'
-		},
-		{
-			name: 'Cool Dude',
-			email: 'cool@dude.com',
-			phone: '333-333-3333'
-		}
-	];
-
-	properties = ['name', 'email', 'phone'];
-	
 })(jQuery);

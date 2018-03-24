30ebcfc24319c03f078d2ead61014d033902f6e6
Fabio Rossetto <fabiorossetto1@gmail.com>
2018-03-23 23:48:48 -0300

    module template file

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+import 'print-js/dist/print.min';
+
+declare type PrintTypes = 'pdf' | 'html' | 'image' | 'json';
+
+export interface Configuration {
+    printable: string;
+    type?: string;
+    documentTitle?: string;
+    htmlData?: string;
+    header?: any;
+    headerStyle?: string;
+    maxWidth?: number;
+    font?: string;
+    font_size?: string;
+    honorMarginPadding?: boolean;
+    honorColor?: boolean;
+    targetStyle?: string | string[];
+    targetStyles?: string | string[];
+    properties?: any;
+    gridStyle?: string;
+    modalMessage?: string;
+    showModal?: boolean;
+    gridHeaderStyle?: string;
+    onLoadingStart?: () => void;
+    onLoadingEnd?: () => void;
+}
+
+declare var printJS: (params: string | Configuration) => void;
+export default printJS;

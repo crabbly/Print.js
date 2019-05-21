a975f75b183516d1f66d22febc9fdbfaa9d2d5ac
Rodrigo Vieira <rodrigo@crabbly.com>
2019-05-21 14:24:23 -0400

    Prevent images from printing in wrong order

--- src/js/image.js
+++ src/js/image.js
@@ -13,54 +13,32 @@ export default {
     let printableElement = document.createElement('div')
     printableElement.setAttribute('style', 'width:100%')
 
-    // Load images and append
-    loadImagesAndAppendToPrintableElement(printableElement, params).then(() => {
-      // Check if we are adding a header
-      if (params.header) addHeader(printableElement, params.header, params.headerStyle)
+    // Create all image elements and append them to the printable container
+    params.printable.forEach(src => {
+      // Create the image element
+      let img = document.createElement('img')
 
-      // Store html data
-      params.htmlData = printableElement.outerHTML
+      // Set image src with the file url
+      img.src = src
 
-      // Print image
-      Print.send(params, printFrame)
-    })
-  }
-}
-
-function loadImagesAndAppendToPrintableElement (printableElement, params) {
-  let promises = []
-
-  params.printable.forEach((image, index) => {
-    // Create the image element
-    let img = document.createElement('img')
-
-    // Set image src with image file url
-    img.src = image
-
-    // Load image
-    promises.push(loadImageAndAppendToPrintableElement(printableElement, params, img, index))
-  })
-
-  return Promise.all(promises)
-}
-
-function loadImageAndAppendToPrintableElement (printableElement, params, img, index) {
-  return new Promise(resolve => {
-    img.onload = () => {
-      // Create image wrapper
+      // Create the image wrapper
       let imageWrapper = document.createElement('div')
       imageWrapper.setAttribute('style', params.imageStyle)
 
-      img.setAttribute('style', 'width:100%;')
-      img.setAttribute('id', 'printableImage' + index)
-
-      // Append image to wrapper element
+      // Append image to the wrapper element
       imageWrapper.appendChild(img)
 
-      // Append wrapper element to printable element
+      // Append wrapper to the printable element
       printableElement.appendChild(imageWrapper)
+    })
 
-      resolve()
-    }
-  })
+    // Check if we are adding a print header
+    if (params.header) addHeader(printableElement, params.header, params.headerStyle)
+
+    // Store html data
+    params.htmlData = printableElement.outerHTML
+
+    // Print image
+    Print.send(params, printFrame)
+  }
 }

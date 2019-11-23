aecc0541fb37290e64b7f79bd392078575813fe7
natalan <belmass@gmail.com>
2019-11-22 19:42:35 -0500

    remove a semicolon that fails linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 

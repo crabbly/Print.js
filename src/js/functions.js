26fe1b303cf4b269caa46406141bc6b4f10317a5
zts <zhangtieshan@chinagci.com>
2021-07-29 16:42:15 +0800

    fix coding style.

--- src/js/functions.js
+++ src/js/functions.js
@@ -86,12 +86,12 @@ export function addFooter (printElement, params) {
 }
 
 export function insterAfter(newElement,targetElement){
-  var parent = targetElement.parentNode;
+  var parent = targetElement.parentNode
   if(parent.lastChild == targetElement){
-    parent.appendChild(newElement);
+    parent.appendChild(newElement)
   }
   else{
-    parent.insertBefore(newElement,targetElement.nextSibling);
+    parent.insertBefore(newElement,targetElement.nextSibling)
   }
 }
 
